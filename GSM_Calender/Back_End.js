aee7cabf03ea5973427c2c3d730d295b93a105fe
박인성 <68880233+parkincastle@users.noreply.github.com>
2021-01-06 18:37:53 +0900

    서버 연결& 도메인

--- GSM_Calender/Back_End.js
+++ GSM_Calender/Back_End.js
@@ -1,3 +1,25 @@
+var http = require('http'); var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈 
+
+// 404 error message : 페이지 오류가 발생했을때, 
+function send404Message(response){ 
+    response.writeHead(404,{"Content-Type":"text/plain"}); // 단순한 글자 출력 
+    response.write("404 ERROR... "); 
+    response.end(); 
+} 
+    
+// 200 Okay : 정상적인 요청 
+function onRequest(request, response){ 
+
+    if(request.method == 'GET' && request.url == '/'){ 
+        response.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력 
+        fs.createReadStream("./index.html").pipe(response); // 같은 디렉토리에 있는 index.html를 response 함 
+    } else { 
+        // file이 존재 하지않을때, 
+        send404Message(response); 
+    } 
+} http.createServer(onRequest).listen(8888); 
+console.log("Server Created...");​
+
 const http = require('http'); const hostname = 'ip주소'; //내 컴퓨터의 IP 
 const port = 3000; 
 const server = http.createServer((req, res) => { 
@@ -9,4 +31,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => { 
     console.log(`Server running at http://${hostname}:${port}/`); 
 });
-
